c0a9dd4845c7a4543ead0ff3ee29042a72612709
majay3574 <majay3574@gmail.com>
2024-01-27 10:36:06 +0530

    Update tests for scrap functionality

--- tests/scrap.spec.ts
+++ tests/scrap.spec.ts
@@ -2,7 +2,7 @@ import test from "@playwright/test"
 test (`Using Keyboard action`,async({page})=>{
 
    await page.goto("https://platform.testleaf.com/#/")
-   await page.waitForTimeout(3000)
+   await page.waitForTimeout(30000)
    
    
 })
