06b0b965ddb06978222111612970ca1b9c59c7f2
majay3574 <majay3574@gmail.com>
2024-02-25 16:32:46 +0530

    committed

--- tests/Day3/01_learn.spec.ts
+++ tests/Day3/01_learn.spec.ts
@@ -1,7 +1,8 @@
 import { chromium, test } from "@playwright/test"
+import { channel } from "diagnostics_channel"
 
 test("title 1", async () => {
-    const browserInstance = await chromium.launch({ headless: false })
+    const browserInstance = await chromium.launch({ headless: false ,channel:"msedge"})
     const browserContext = await browserInstance.newContext()
     const page = await browserContext.newPage()
     await page.goto("https://www.google.com")
--- tests/Day3/02_learn.spec.ts
+++ tests/Day3/02_learn.spec.ts
@@ -2,9 +2,10 @@ import test, { chromium } from "@playwright/test"
 
 
 test(`launching browser`,async({})=>{
-    const browser=await chromium.launch({headless:false,channel:'chrome'})
+    const browser=await chromium.launch({channel:'chrome'})
     const context= await browser.newContext()
     const page= await context.newPage()
     await page.goto("https://artsandculture.google.com/project")
     console.log(page.url())
+    console.log(await page.title())
 })
