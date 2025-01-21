94215eac41ed2f1bd8d1272065f5c679833e574d
majay3574 <majay3574@gmail.com>
2025-01-21 14:38:40 +0530

    Renamed and fixed rep

--- tests/Day6/function_parametraion.ts
+++ tests/Day6/function_parametraion.ts
@@ -3,9 +3,9 @@ function signup1(username: string, password: string) {
     console.log("Signing up with password:", password);
 }
 
-function signup2(email: string, password: string) {
+function signup2(email: string) {
     console.log("Signing up with email:", email);
 }
 
 signup1("majay@gmail.com", "pass"); // Calls signup1
-signup2("majay3574@gmail.com", "pass"); // Calls signup2
+signup2("majay3574@gmail.com"); // Calls signup2
